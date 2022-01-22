19b1a8640e57eaab34263ff225b24829f62b9ae5
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-01-23 00:00:15 +0530

    Used for exporting the package

---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+import Text from './Components/Text';
+import Child from './Components/Child';
+import Image from './Components/Image';
+import Container from './Components/Container';
+
+export {Text, Child, Image, Container};
