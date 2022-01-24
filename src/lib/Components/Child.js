9864b878aaa8d934e1cdbc57d111c97bff653787
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-01-24 14:14:36 +0530

    Adding custom stylesheet

--- src/lib/Components/Child.js
+++ src/lib/Components/Child.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Text from './Text';
 import Image from './Image';
+import "../Styles/style.css";
 import PropTypes from 'prop-types';
 import Container from './Container';
 
