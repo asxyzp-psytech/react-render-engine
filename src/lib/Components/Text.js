ec095d7791e32d93a5fdad279a96918b767638bd
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-01-24 14:05:03 +0530

    + l25 [TYPO] : Getting text styles from element.text instead of element.design + l31 [ADDITION]: Adding condition for adding class value for layout elements +l34 [ADDITION]: Adding condition for adding child element for Text +l38 [REPLACE]: Changing how textual content is rendered.

--- src/lib/Components/Text.js
+++ src/lib/Components/Text.js
@@ -1,5 +1,6 @@
 //IMPORTING MODULES/PACKAGES
 import React from 'react';
+import "../Styles/style.css";
 import PropTypes from 'prop-types';
 
 /**
@@ -22,21 +23,24 @@ const Text = (props) => {
     //GETTING ELEMENT STYLES
     const textStyleId = element.text;                                                  //ID OF TEXT STYLE ELEMENT
     const designStyleId = element.design;                                              //ID OF DESIGN STYLE ELEMENT
-    const textStyle = template.design[textStyleId];                                    //TEXT STYLE OBJECT
+    const textStyle = template.text[textStyleId];                                      //TEXT STYLE OBJECT
     const designStyle = template.design[designStyleId];                                //DESIGN STYLE OBJECT
-    const styles = { ...inputStyle, ...textStyle, ...designStyle };                      //CONTAINER STYLE = (INPUT + TEXT + DESIGN) STYLE OBJECT
+    const styles = { ...inputStyle, ...textStyle, ...designStyle };                    //CONTAINER STYLE = (INPUT + TEXT + DESIGN) STYLE OBJECT
 
     //GETTING ELEMENT PROPERTIES (CLASSES)
     const properties = element['properties'];                                          //CONTAINS CLASSES AND ATTRIBUTES
-    const classes = properties.filter(property => !property.includes("=")).join(' ');  //STORES CLASSES OF CONTAINER ELEMENT: ['a', 'b', 'c=d'] => 'a b'
+    const classes = properties.filter(property => !property.includes("=")).length>0?
+                    properties.filter(property => !property.includes("=")).join(' '):'';  //STORES CLASSES OF CONTAINER ELEMENT: ['a', 'b', 'c=d'] => 'a b'
 
     //GETTING LIST OF CHILDREN OF THE ELEMENT;
-    const children = element.children;
+    const child = element.children.length>0?element.children[0]:'';
 
     return (
-        <div style={styles} className={classes}>
-            {children[0]}
-        </div>
+        <div style={styles}
+            className={classes}
+            //SECURITY RISK: NEED TO CHANGE THIS TO INCLUDE ICON ELEMENT
+            dangerouslySetInnerHTML={{ __html: child }}></div>
+
     );
 };
 
